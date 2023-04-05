dc77642c4dcb5f3fcc25cde085d731982ed037d4
safirasn21 <safirasn021@gmai.com>
2023-04-05 13:10:32 +0700

    first commit

---
+++ struct3/struct3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukan nim : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "Masukan nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\n\tMasukan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukan kota : ";
+		cin.getline(mhs[i].alamat.kota, 10);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << "\n\nnNIM : " << mhs[i].nim;
+		cout << "\nUmur : " << mhs[i].umur;
+		cout << "\nnama : " << mhs[i].nama;
+		cout << "\nAlamat : ";
+		cout << "\n\tDesa : " << mhs[i].alamat.desa;
+		cout << "\n\tKota : " << mhs[i].alamat.kota;
+	}
+
+}
